5ebb526c838cec879d69cc796df2de31fbe21117
Nicholas Jones <nicholassjones2003@gmail.com>
2024-12-15 17:58:50 -0600

    Experimenting with Unity game adding

---
+++ practice.js
@@ -0,0 +1,7 @@
+const youTubeBut = document.getElementById('youTubeBut');
+
+button.addEventListener('click', goToYoutube);
+
+function goToYoutube() {
+    window.location.href = 'https://www.youtube.com';
+}
