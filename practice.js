1e06aad60172489b8f93ed95cb633c57d4627ceb
Nicholas Jones <nicholassjones2003@gmail.com>
2025-01-05 14:43:17 -0600

    Scrubbing everything and starting from a template

--- practice.js
+++
@@ -1,7 +0,0 @@
-const youTubeBut = document.getElementById('youTubeBut');
-
-button.addEventListener('click', goToYoutube);
-
-function goToYoutube() {
-    window.location.href = 'https://www.youtube.com';
-}
